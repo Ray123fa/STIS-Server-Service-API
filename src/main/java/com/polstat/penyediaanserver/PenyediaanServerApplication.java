d8939d6b499d2545d7a4be18e2429b38773bf202
Rayhan F <rayhanfrdh123@gmail.com>
2024-11-09 23:48:55 +0700

    Initial project

---
+++ src/main/java/com/polstat/penyediaanserver/PenyediaanServerApplication.java
@@ -0,0 +1,13 @@
+package com.polstat.penyediaanserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PenyediaanServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PenyediaanServerApplication.class, args);
+    }
+
+}
